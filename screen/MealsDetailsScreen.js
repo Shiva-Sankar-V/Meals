585ea9945ac2aba4a1d85cd5dd223f428e21abae
shiva <shivasankarvenkat1996@gmail.com>
2023-05-26 22:52:36 +0530

    providing data to detail screen and styling

--- screen/MealsDetailsScreen.js
+++ screen/MealsDetailsScreen.js
@@ -1,13 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
 
-const MealsDetailsScreen = () => {
+const MealsDetailsScreen = ({ route }) => {
+  const mealID = route.params.mealId;
+
   return (
-    <View>
-      <Text>MealsDetailsScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>This is meal detail screen.</Text>
+      <Text style={styles.text}>Select meal has an ID:({mealID})</Text>
     </View>
   );
 };
 
 export default MealsDetailsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
