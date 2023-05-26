b3f0feb0cd563da2b8b33ebcf266b1abf3a1741f
shiva <shivasankarvenkat1996@gmail.com>
2023-05-26 17:52:19 +0530

    created different comp for meals list

--- screen/MealsOverviewScreen.js
+++ screen/MealsOverviewScreen.js
@@ -1,11 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, FlatList, View } from "react-native";
 import { MEALS } from "../data/dummyData";
+import MealItem from "../components/MealItem";
 
 const MealsOverviewScreen = ({ route }) => {
   const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
