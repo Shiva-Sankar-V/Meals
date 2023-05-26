5e49c56c44372c1002fae344bcf9ddb9d94b4970
shiva <shivasankarvenkat1996@gmail.com>
2023-05-26 14:55:21 +0530

    changed status bar style to light

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
+import { StatusBar } from "expo-status-bar";
 import CategoriesScreen1 from "./screen/CategoriesScreen1";
 import { StyleSheet } from "react-native";
 
 export default function App() {
-  return <CategoriesScreen1 />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen1 />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
