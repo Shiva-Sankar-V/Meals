6eff662e6d3ce5b57a40c97ce22f5c859aa2da7b
shiva <shivasankarvenkat1996@gmail.com>
2023-05-26 14:48:58 +0530

    grid style

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { StyleSheet, Text, View, Pressable, Platform } from "react-native";
 
 const CategoryGridTile = (props) => {
   return (
-    <View style={styles.gridItem}>
-      <Pressable style={styles.button}>
+    <View style={[styles.gridItem, { backgroundColor: props.color }]}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={(pressed) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
         <View style={styles.innerContainer}>
-          <Text>{props.title}</Text>
+          <Text style={styles.title}>{props.title}</Text>
         </View>
       </Pressable>
     </View>
@@ -17,18 +23,36 @@ export default CategoryGridTile;
 const styles = StyleSheet.create({
   gridItem: {
     flex: 1,
-    margin: 16,
+    marginHorizontal: 16,
+    marginVertical: 12,
     height: 150,
-    borderRadius: 8,
+    borderwidth: 5,
+    borderColor: "red",
+    borderRadius: 12,
     elevation: 4,
+    overflow: Platform.OS === "android" ? "hidden" : "visible", //to make sure android ripple effect not go beyond margin and shadow visible in ios
+
+    // backgroundColor:'white',    //shadow effect for ios
+    // shadowColor: "black",
+    // shadowOpacity: 0.25,
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowRadius: 8,
   },
   button: {
     flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.75,
   },
   innerContainer: {
     flex: 1,
     padding: 16,
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
   },
 });
