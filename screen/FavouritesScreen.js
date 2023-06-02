d7b7a2367511c19e751c46bcc7d0cff39438a9fa
shiva <shivasankarvenkat1996@gmail.com>
2023-06-02 13:19:12 +0530

    added a button if there are no favorites

--- screen/FavouritesScreen.js
+++ screen/FavouritesScreen.js
@@ -1,13 +1,32 @@
+import { View, Text, StyleSheet, Button } from "react-native";
 import { useContext } from "react";
 import MealList from "../components/MealList/MealList";
 import { FavoritesContext } from "../store/Context1/FavoritesContext";
 import { MEALS } from "../data/dummyData";
 
-const FavouritesScreen = () => {
+const FavouritesScreen = ({ navigation }) => {
   const favMealCtx = useContext(FavoritesContext);
   const favMeals = MEALS.filter((meal) => favMealCtx.ids.includes(meal.id));
+  if (favMeals.length === 0) {
+    return (
+      <View style={style.rootContainer}>
+        <Button
+          title="Add Favorites"
+          onPress={() => navigation.navigate("Categories")}
+        />
+      </View>
+    );
+  }
 
   return <MealList item={favMeals} />;
 };
 
 export default FavouritesScreen;
+
+const style = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
