c6cc0d662d20237c692a7aa2b570e814b71fa3ac
shiva <shivasankarvenkat1996@gmail.com>
2023-06-02 12:26:05 +0530

    sending data to favorite screen & displaying them

--- screen/FavouritesScreen.js
+++ screen/FavouritesScreen.js
@@ -1,14 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { useContext } from "react";
+import MealList from "../components/MealList/MealList";
+import { FavoritesContext } from "../store/Context1/FavoritesContext";
+import { MEALS } from "../data/dummyData";
 
 const FavouritesScreen = () => {
-  return (
-    <View>
-      <Text>Favourites Screen</Text>
-    </View>
-  );
+  const favMealCtx = useContext(FavoritesContext);
+  const favMeals = MEALS.filter((meal) => favMealCtx.ids.includes(meal.id));
+
+  return <MealList item={favMeals} />;
 };
 
 export default FavouritesScreen;
-
-const styles = StyleSheet.create({});
