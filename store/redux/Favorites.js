bb8944b69e5efafc15b92fc38ccd0942cd5663f3
shiva <shivasankarvenkat1996@gmail.com>
2023-06-02 16:41:40 +0530

    additing conditions for updating state

---
+++ store/redux/Favorites.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const favSlice = createSlice({
+  name: "favorites",
+  initialState: {
+    ids: [],
+  },
+  reducers: {
+    addFav: (state, action) => {
+      state.ids.push(action.payload.id);
+    },
+    removeFav: (state, action) => {
+      state.ids.splice(state.ids.indexOf(action.payload.id), 1);
+    },
+  },
+});
+
+export const addFavorite = favSlice.actions.addFav;
+export const remFavorite = favSlice.actions.removeFav;
+export default favSlice.reducer;
